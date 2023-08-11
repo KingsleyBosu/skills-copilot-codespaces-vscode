3b9d7073fdb232a989a13f9bfd1417f925908d47
Kingsley Bosu <55680575+KingsleyBosu@users.noreply.github.com>
2023-08-11 01:59:38 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,71 @@
+// Create web server
+// Create a route for the path /comments
+// Send the following JSON data as a response: { "yourComment": "This is my first comment" }
+const express = require('express');
+const app = express();
+
+app.get('/comments', (req, res) => {
+  res.json({ "yourComment": "This is my first comment" });
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000!');
+});
+
+// Path: comments.js
+// Create web server
+// Create a route for the path /comments
+// Send the following JSON data as a response: { "yourComment": "This is my first comment" }
+const express = require('express');
+const app = express();
+
+app.get('/comments', (req, res) => {
+  res.json({ "yourComment": "This is my first comment" });
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000!');
+});
+
+// Path: comments.js
+// Create web server
+// Create a route for the path /comments
+// Send the following JSON data as a response: { "yourComment": "This is my first comment" }
+const express = require('express');
+const app = express();
+
+app.get('/comments', (req, res) => {
+  res.json({ "yourComment": "This is my first comment" });
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000!');
+});
+
+// Path: comments.js
+// Create web server
+// Create a route for the path /comments
+// Send the following JSON data as a response: { "yourComment": "This is my first comment" }
+const express = require('express');
+const app = express();
+
+app.get('/comments', (req, res) => {
+  res.json({ "yourComment": "This is my first comment" });
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000!');
+});
+
+// Path: comments.js
+// Create web server
+// Create a route for the path /comments
+// Send the following JSON data as a response: { "yourComment": "This is my first comment" }
+const express = require('express');
+const app = express();
+
+app.get('/comments', (req, res) => {
+  res.json({ "yourComment": "This is my first comment" });
+});
+
+app
